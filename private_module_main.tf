ea5b70f4f49dba2321ed176e8f8b53695f2887fe
Max Amelchenko <maxamel2002@gmail.com>
2025-02-12 14:20:11 +0200

    Update private_module_main.tf

--- private_module_main.tf
+++ private_module_main.tf
@@ -16,6 +16,21 @@ module "ec4" {
   subnet_id   = "subnet-12345678"       
 }
 
+module "ec6" {
+  source      = "git::https://github.com/maxamel/test_module3.git"
+  ami_id      = "ami-0c55b159cbfafe1f0" 
+  instance_type = "t2.micro"
+  subnet_id   = "subnet-12345678"       
+}
+
 output "ec2_instance_id" {
   value = module.ec2.instance_id
+}
+
+output "ec4_instance_id" {
+  value = module.ec4.instance_id
+}
+
+output "ec6_instance_id" {
+  value = module.ec6.instance_id
 }
